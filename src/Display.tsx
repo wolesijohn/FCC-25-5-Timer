afa5c28c285cff05eed2a1554e7f3dcaf1e67ffc
TOSHIBA <wolesijohn@gmail.com>
2024-11-27 21:52:38 +0100

    clock project

---
+++ src/Display.tsx
@@ -0,0 +1,34 @@
+import {FaPause, FaPlay, FaUndo} from "react-icons/fa";
+import { DisplayState, formatTime } from "./helpers";
+interface DisplayProps{
+    displayState:DisplayState;
+    reset: ()=>void;
+    startStop:(displayState:DisplayState)=>void;
+}
+
+
+const Display: React.FC<DisplayProps> =({
+    displayState,
+    reset,
+    startStop,
+})=>{
+    return(
+        <div className="display"> 
+            <h4 id="timer-label">{displayState.timeType}</h4>
+            <span id="time-left" 
+            style={{color:`${displayState.timeRunning? "red":"white"}`}}>{formatTime(displayState.time)}
+
+            </span>
+            <div>
+                <button id="start_stop" onClick={()=>startStop(displayState)}>.
+                    {displayState.timeRunning? <FaPause />:<FaPlay />}
+                </button>
+                <button id="reset" onClick={reset}>
+                    <FaUndo />
+                </button>
+            </div>
+        </div>
+        )
+    
+    }
+export default Display
